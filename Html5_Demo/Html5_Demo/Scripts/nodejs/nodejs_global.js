7e5d092a3b0a6f4dcddfc82617aa7b31646bb67c
dupengsim <dupengsim@163.com>
2017-05-24 16:16:08 +0800

    nodejs util工具

---
+++ Html5_Demo/Html5_Demo/Scripts/nodejs/nodejs_global.js
@@ -0,0 +1,55 @@
+﻿
+/*
+ *  nodejs 全局对象与全局变量
+ */
+
+// 返回当前模块文件的绝对路径
+console.log(__filename);
+
+// 返回当前执行脚本 所在的目录
+console.log(__dirname);
+
+// setTimeout定时器
+{
+    function printHello() {
+        console.log("Hello  World！");
+    }
+    setTimeout(printHello, 2000);
+}
+
+// clearTimeout
+{
+    function printHello2() {
+        console.log("Hello World !!!!");
+    }
+    var t = setTimeout(printHello, 2000);
+    clearTimeout(t);
+}
+
+// process
+process.on('exit', function (code) {
+    // 以下代码永远不会执行
+    setTimeout(function () {
+        console.log("此处代码不会执行");
+    }, 0);
+    console.log("退出码是：", code);
+});
+console.log("程序执行结束");
+
+// 输出到终端
+process.stdout.write("Hello World!" + "\n");
+// 通过参数读取
+process.argv.forEach(function (val, index, array) {
+    console.log(index + "：" + val);
+});
+// 获取执行路径
+console.log(process.execPath);
+// 输出平台信息
+console.log(process.platform);
+
+// 输出当前目录
+console.log("当前目录：" + process.cwd());
+// 输出当前版本
+console.log("当前版本：" + process.version);
+// 输出内存使用情况
+console.log(process.memoryUsage());
---
+++ Html5_Demo/Html5_Demo/Scripts/nodejs/nodejs_util.js
@@ -0,0 +1,43 @@
+﻿
+/*
+ * nodejs常用的工具
+ */
+
+var util = require('util');
+function Base() {
+    this.name = "base";
+    this.birth = 1989;
+    this.sayHello = function () {
+        console.log("Hello " + this.name);
+    };
+}
+
+Base.prototype.showName = function () {
+    console.log(this.name);
+};
+
+function Sub() {
+    this.name = "sub";
+}
+
+util.inherits(Sub, Base);
+var objBase = new Base();
+objBase.showName();
+objBase.sayHello();
+console.log(objBase);
+
+var objSub = new Sub();
+objSub.showName();
+//objSub.sayHello();
+console.log(objSub);  // 子类只能继承原型中定义的属性和方法，内部构造函数中定义的属性和函数不会被继承
+
+
+function Person() {
+    this.name = "dupeng";
+    this.toString = function () {
+        return this.name;
+    };
+}
+var obj = new Person();
+console.log(util.inspect(obj));
+console.log(util.inspect(obj, true));
